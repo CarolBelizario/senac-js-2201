3bdef1a387c3901d427c69f1d801e2f88d1a9796
CAROLINA VIEIRA BELIZARIO <carolina.vbelizario@senacsp.edu.br>
2022-02-16 23:15:41 -0200

    aula2js

---
+++ app.js
@@ -0,0 +1,21 @@
+//comentário de linha
+
+/*
+Comentário de bloco
+*/
+
+const XPTO = 10;
+
+console.log(XPTO);
+
+let minhaVar = 'Olá';
+
+minhaVar = minhaVar + ' Mundo!';
+
+console.log(minhaVar);
+
+
+
+function diretoNoElemento(){
+    console.log('Olá mundo!');
+}
