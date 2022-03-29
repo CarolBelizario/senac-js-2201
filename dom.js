e7f718566d7c246d522e54c73ec39632709cedaa
Carolina Belizário <99371235+CarolBelizario@users.noreply.github.com>
2022-03-29 20:17:40 -0300

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-const p = document.getElementById('paragrafo');
+const p = document.getElementById('paragrafo');//retorna valor(texto) inserido pelo usuário
 
 p.innerText = 'Meu texto!';
 
@@ -8,7 +8,7 @@ for(i=0, c = 0 ; i < 100; i++, c++) {
 
     p.style.background = colors[c];
     
-    console.log(c + ': ' + p.style.background);
+    console.log(c + ': ' + p.style.background);//vai exibir no console uma ultramegarápida troca de cores a cada laço
 
     if(c>= 6) c = -1;
 
